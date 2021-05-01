3640ecdacfb8f8d4fa18fab3cfc06585503c08b5
Karthik Nair <78267371+realKarthikNair@users.noreply.github.com>
2021-05-01 21:58:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ def path(system):
         print(f"(default is {path} )")
     else: #for non-nt (posix ; macos not included)"
         path = os.path.join(os.environ['HOME'],"gboard_shortcuts")
-        print(path[0:4])
         if "/data" in path[0:5]: #if android
             path="/storage/emulated/0/gboard_shortcuts"
         print(f"(default is {path} )")
     try:
-        input_path = input("Enter here : ")
+        if system=='posix':print(f"The home dircetory on your system is {os.environ['HOME']} ")
+        input_path=input("Enter path : ")
         if not os.path.exists(input_path):raise Exception
         path=input_path
-        return path
+        return path,system
     except:
         #if not default():print("invalid path : try again ! ");path(os.name,default=False)
-        print(f"invalid path ....\n choosing directory as {path}")
+        print(f"path doesnt exist (invalid !) ....\n choosing directory as {path}")
         try:os.makedirs(path)
         except:pass
         return path,system
@@ -60,10 +60,10 @@ def dict_create(path,shortcuts,status):
         if path[1]=="nt":
             file_name=path[0]+"\shortcuts-"+datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")+".txt"
         else:
-            file_name=os.path.join(path[0],"\shortcuts-"+datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")+".txt")
+            file_name=os.path.join(path[0],"shortcuts-"+datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")+".txt")
         dict_txt=open(file_name,"w")
-        ###############################################################################################################
-############################################################
+        
+######################################################################################################################
 #    if status.lower()=="add":
 #        print(f"reading files from {path} . . . . .")
 #        #os.path.isfile
@@ -76,7 +76,7 @@ def dict_create(path,shortcuts,status):
 #        choice=int(input("Enter here : "))
 #        if choice=='':path=path(os.name);
 #        
-        ###############################################################################################################
+#######################################################################################################################
     dict_txt.writelines(shortcuts);dict_txt.close()
     path = zipper(path[0],os.path.basename(file_name),status)
     print(f"success! saved file as {os.path.basename(path)} dictionary file in {os.path.dirname(path)} ")
@@ -97,4 +97,3 @@ while True:
         else:dict_create(store_path, shortcut_maker(choice), choice)
     elif choice.lower() == "exit":print("Thank you for using the program \n Exiting...... ");break
     else:print("invalid option : please try again ");continue
-
