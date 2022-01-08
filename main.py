a0afed03d28f2c1d7c76591633dcdae39c1f06bd
realkarthiknair <knairothera@gmail.com>
2022-01-08 18:02:48 +0530

    t

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ selected = False
 entry = "None"
 nothing="var"
 
+
+
 def save(entry):
     """ Saves the Word and Shortcut Entered (if user don't want to add more words) """
     global words, shortcuts, path, total_words, total_shortcuts
